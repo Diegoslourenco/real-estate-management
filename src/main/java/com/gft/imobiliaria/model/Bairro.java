aafe427e2b4685349b67a5dd112b8501127de256
dodn <diego.lourenco@gft.com>
2021-04-29 09:25:28 -0300

    remove unused imports in Bairro

--- src/main/java/com/gft/imobiliaria/model/Bairro.java
+++ src/main/java/com/gft/imobiliaria/model/Bairro.java
@@ -1,6 +1,5 @@
 package com.gft.imobiliaria.model;
 
-import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
